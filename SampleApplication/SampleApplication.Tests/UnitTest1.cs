9a5e2d9ebbea981fd89532479b69b6bc3115741d
Vlad Munthiu <Vlad.Munthiu@quest.com>
2020-01-13 17:56:46 +0000

    initial check in

---
+++ SampleApplication/SampleApplication.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using Xunit;
+
+namespace SampleApplication.Tests
+{
+    public class UnitTest1
+    {
+        public class Function1Test
+        {
+            private readonly ILogger logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+            [Fact]
+            public async void HttpTriggerWithParams()
+            {
+                var request = TestFactory.CreateHttpRequest("name", "Bill");
+                var response = (OkObjectResult)await Function1.Run(request, logger);
+                Assert.Equal("Hello Bill! Welcome to Azure Functions!", response.Value);
+            }
+
+            [Fact]
+            public async void HttpTriggerWithoutParams()
+            {
+                var request = TestFactory.CreateHttpRequest("", "");
+                var response = (OkObjectResult)await Function1.Run(request, logger);
+                Assert.Equal("Hello there! Welcome to Azure Functions!", response.Value);
+            }
+        }
+    }
+}
