4fd713c9f81df5e30cce3525bfd0856f18efac17
Vlad Munthiu <Vlad.Munthiu@quest.com>
2020-01-14 15:30:14 +0000

    improve logging

--- SampleApplication/SampleApplication/Function1.cs
+++ SampleApplication/SampleApplication/Function1.cs
@@ -17,7 +17,7 @@ namespace SampleApplication
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request. new log info");
 
             string name = req.Query["name"];
 
