299bc14fb1f441a7ac8b215198fa029d892d148b
Vlad Munthiu <Vlad.Munthiu@quest.com>
2020-01-21 17:55:39 +0000

    fix functional tests

--- SampleApplication/SampleApplication.FunctionalTests/UnitTest1.cs
+++ SampleApplication/SampleApplication.FunctionalTests/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace SampleApplication.FunctionalTests
         public void CallFuntion1WithParams()
         {
             var client = new RestClient("https://sampleapp-dev.azurewebsites.net/");
-            var request = new RestRequest("api/Function1?code=b0DHJDALasLuuAEE7b/j/6t6NsWST4gXGK6VZ7MYrqbITwa1bTp4WQ==&name=Bill", Method.GET);
+            var request = new RestRequest("api/Function1?code=Sa3tAomy3oZaG4I9Ska14wkUnsdFVbvS7YB2eSLaA7hL9vmKXgRs1w==&name=Bill", Method.GET);
             var result = client.Execute(request);
             Assert.Equal("\"Hello Bill! Welcome to Azure Functions!\"", result.Content);
         }
@@ -19,7 +19,7 @@ namespace SampleApplication.FunctionalTests
         public void CallFuntion1WithoutParams()
         {
             var client = new RestClient("https://sampleapp-dev.azurewebsites.net/");
-            var request = new RestRequest("api/Function1?code=b0DHJDALasLuuAEE7b/j/6t6NsWST4gXGK6VZ7MYrqbITwa1bTp4WQ==", Method.GET);
+            var request = new RestRequest("api/Function1?code=Sa3tAomy3oZaG4I9Ska14wkUnsdFVbvS7YB2eSLaA7hL9vmKXgRs1w==", Method.GET);
             var result = client.Execute(request);
             Assert.Equal("\"Hello there! Welcome to Azure Functions!\"", result.Content);
         }
